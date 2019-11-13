9b23cbd6a296ddd6f69c27d0f2418dc9d1bbf6b9
Uladzimir Paliashuk <Uladzimir_Paliashuk@epam.com>
2019-11-13 19:25:27 +0300

    Task 3-2

---
+++ asp.net-core-belarus/Components/BreadCrumbsWidget.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace asp.net_core_belarus.Components
+{
+    public class BreadCrumbsWidget : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string s = HttpContext.Request.Path.ToString().Replace("/"," > ");
+            s = Regex.Replace(s, @"> 0", " > Create New");
+            s = Regex.Replace(s, @"> [0-9]", "");
+
+            return View((object) s);
+        }
+    }
+}
