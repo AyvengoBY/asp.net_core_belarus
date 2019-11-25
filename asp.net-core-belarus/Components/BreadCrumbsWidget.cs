a16dc0ade57d9e3fa5669fd86365d5a996463a9b
Uladzimir Paliashuk <Uladzimir_Paliashuk@epam.com>
2019-11-25 17:28:48 +0300

    Added ability to click on BreadCrumbsWidget

--- asp.net-core-belarus/Components/BreadCrumbsWidget.cs
+++ asp.net-core-belarus/Components/BreadCrumbsWidget.cs
@@ -12,10 +12,17 @@ namespace asp.net_core_belarus.Components
         public async Task<IViewComponentResult> InvokeAsync()
         {
             string s = HttpContext.Request.Path.ToString().Replace("/"," > ");
-            s = Regex.Replace(s, @"> 0", " > Create New");
-            s = Regex.Replace(s, @"> [0-9]", "");
-
-            return View((object) s);
+            string spath = HttpContext.Request.Path.ToString();
+            spath = Regex.Replace(spath, @"/[0-9]", "");
+            SortedDictionary<string, string> path = new SortedDictionary<string,string>();
+            
+            while(spath.Contains("/"))
+            {
+                string name = Regex.Match(spath, @"\/([A-Z][a-z]+)+$").ToString().Replace("/","");
+                path.Add(name, spath);
+                spath = Regex.Replace(spath, @"\/([A-Z][a-z]+)+$", "");
+            }
+            return View(path);
         }
     }
 }
